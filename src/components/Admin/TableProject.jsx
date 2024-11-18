cc9790483dab5c7081fd15ddafeebf726e491d73
Perdana Main <firman.fp123@gmail.com>
2024-11-18 09:41:07 +0700

    refactor:  dashboard

---
+++ src/components/Admin/TableProject.jsx
@@ -0,0 +1,94 @@
+import {
+  Table,
+  TableHeader,
+  TableBody,
+  TableColumn,
+  TableRow,
+  TableCell,
+  getKeyValue,
+} from "@nextui-org/table";
+import React from "react";
+
+const TableProject = () => {
+  const rows = [
+    {
+      key: "1",
+      name: "Tony Reichert",
+      role: "CEO",
+      status: "Active",
+    },
+    {
+      key: "2",
+      name: "Zoey Lang",
+      role: "Technical Lead",
+      status: "Paused",
+    },
+    {
+      key: "3",
+      name: "Jane Fisher",
+      role: "Senior Developer",
+      status: "Active",
+    },
+    {
+      key: "4",
+      name: "William Howard",
+      role: "Community Manager",
+      status: "Vacation",
+    },
+  ];
+
+  const columns = [
+    {
+      key: "name",
+      label: "NAME",
+    },
+    {
+      key: "role",
+      label: "ROLE",
+    },
+    {
+      key: "status",
+      label: "STATUS",
+    },
+  ];
+
+  const classNames = React.useMemo(
+    () => ({
+      wrapper: ["max-h-[382px]", "max-w-3xl"],
+      th: ["bg-transparent", "text-default-500", "border-b", "border-divider"],
+      td: [
+        // changing the rows border radius
+        // first
+        "group-data-[first=true]:first:before:rounded-none",
+        "group-data-[first=true]:last:before:rounded-none",
+        // middle
+        "group-data-[middle=true]:before:rounded-none",
+        // last
+        "group-data-[last=true]:first:before:rounded-none",
+        "group-data-[last=true]:last:before:rounded-none",
+      ],
+    }),
+    []
+  );
+  return (
+    <Table
+      aria-label="Example table with dynamic content"
+      classNames={classNames}
+    >
+      <TableHeader columns={columns}>
+        {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+      </TableHeader>
+      <TableBody items={rows}>
+        {(item) => (
+          <TableRow key={item.key}>
+            {(columnKey) => (
+              <TableCell>{getKeyValue(item, columnKey)}</TableCell>
+            )}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
+};
+
+export default TableProject;
--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,7 +1,48 @@
-import AdminLayout from "../../containers/AdminLayout";
+import { Listbox, ListboxItem } from "@nextui-org/react";
+import React from "react";
+import Hamburger from "../../assets/icons/List.png";
+import Stars from "../../components/Stars";
+import MainLayout from "../../containers/MainLayout";
+import TableProject from "../../components/Admin/TableProject";
 
 const Dashboard = () => {
-  return <AdminLayout>Dashboard</AdminLayout>;
+  const [menuOpen, setMenuOpen] = React.useState(false);
+
+  return (
+    <MainLayout>
+      <div className="container mx-auto relative z-10">
+        <Stars />
+
+        <div className="flex my-10">
+          {/* Konten yang tersembunyi di layar kecil dan muncul di layar besar */}
+
+          <Listbox
+            aria-label="Actions"
+            onAction={(key) => alert(key)}
+            className={`flex-auto ${
+              menuOpen ? "block" : "hidden"
+            } sm:block mx-3 w-5 p-4 border-[1px] rounded-2xl bg-white`}
+          >
+            <ListboxItem key="new">New file</ListboxItem>
+            <ListboxItem key="copy">Copy link</ListboxItem>
+            <ListboxItem key="edit">Edit file</ListboxItem>
+            <ListboxItem key="delete" className="text-danger" color="danger">
+              Delete file
+            </ListboxItem>
+          </Listbox>
+
+          <div className="flex-auto w-96 rounded-2xl p-4">
+            <div className="sm:hidden">
+              <button onClick={() => setMenuOpen(!menuOpen)}>
+                <img src={Hamburger} alt="Menu" />
+              </button>
+            </div>
+            <TableProject />
+          </div>
+        </div>
+      </div>
+    </MainLayout>
+  );
 };
 
 export default Dashboard;
